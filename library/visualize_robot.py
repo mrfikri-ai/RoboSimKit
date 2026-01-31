c38303c259279bccd407681c08b966e124063e1d
Muhamad Rausyan Fikri <9569417+mrfikri-ai@users.noreply.github.com>
2026-01-31 21:23:45 +0200

    Initialize trajectory plot and time display
    Add initial trajectory plot and time stamp display.

--- library/visualize_robot.py
+++ library/visualize_robot.py
@@ -12,5 +12,8 @@ class sim_mobile_robot:
     self.ax.set(xlabel = "x [m]", ylabel = "y [m]")
     self.ax.set_aspect('equal', adjustable ='box', anchor='C')
     plt.tight_layout()
-
-#
+    
+    # Plot initial value for trajectory and time stamp
+    self.traj_pl, = self.ax.plot(0,0, 'b--')
+    self.time_txt = self.ax.text(0.78, 0.01, 't = 0 s', color = 'k', fontsize = 'large'
+        horizontalalignment = 'left', verticalalignment='bottom', transform = self.ax.transAxes)
